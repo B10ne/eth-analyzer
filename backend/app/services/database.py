aa8dd29fd86431bf103a409317dca04abb5c8d6c
B10ne <b10nejune@gmail.com>
2025-09-24 19:59:00 +0700

    Lanjutkan pembaruan frontend dan backend

--- backend/app/services/database.py
+++ backend/app/services/database.py
@@ -1,13 +1,14 @@
 # backend/app/services/database.py
 import mysql.connector
+import os # <-- Tambahkan ini
 
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="eth_analyzer_db"
+            host=os.environ.get("MYSQL_HOST"),      # Ambil dari variabel lingkungan
+            user=os.environ.get("MYSQL_USER"),      # Ambil dari variabel lingkungan
+            password=os.environ.get("MYSQL_PASSWORD"),  # Ambil dari variabel lingkungan
+            database=os.environ.get("MYSQL_DATABASE") # Ambil dari variabel lingkungan
         )
         return conn
     except mysql.connector.Error as err:
